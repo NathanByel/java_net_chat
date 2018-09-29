efab53b8f70370885bc8f02c68010d518ed99472
SERVER\TriZet <n-k86@mail.ru>
2018-09-29 20:13:37 +0300

    Добавлена форма регистрации.

--- src/com/java_net_chat/Client/LoginWindow.java
+++ src/com/java_net_chat/Client/LoginWindow.java
@@ -7,11 +7,13 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 public class LoginWindow extends JFrame implements ClientUI {
-    private static final String INPUT_NICK_TEXT = "Введите никнейм...";
-    private static final String INPUT_PASS_TEXT = "Введите пароль...";
+    private static final String INPUT_NICK_TEXT         = "Введите никнейм...";
+    private static final String INPUT_PASS_TEXT         = "Введите пароль...";
+    private static final String INPUT_PASS_CONFIRM_TEXT = "Подтверждение пароля...";
     private JTextField nickNameField;
     private JPasswordField passwordField;
-    private JButton buttonIn;
+    private JPasswordField passwordConfirmField;
+    private JButton buttonLogIn;
     private ClientController clientController;
 
     public LoginWindow(ClientController clientController) {
@@ -23,22 +25,45 @@ public class LoginWindow extends JFrame implements ClientUI {
         setLocationRelativeTo(null);
         setLayout( new BoxLayout(getContentPane(), BoxLayout.Y_AXIS) );
 
-
+        // Поле ник
         nickNameField = new JTextField();
         nickNameField.setText(INPUT_NICK_TEXT);
         add(nickNameField);
 
+        // Поле пароль
         passwordField = new JPasswordField();
         passwordField.setEchoChar('\u0000'); // Делаем текст видимым
         passwordField.setText(INPUT_PASS_TEXT);
         add(passwordField);
 
-        buttonIn = new JButton();
-        buttonIn.setAlignmentX(CENTER_ALIGNMENT);
-        buttonIn.setText("Войти");
-        //buttonIn.setFocusPainted(false);
-        //buttonIn.requestFocusInWindow();
-        add(buttonIn);
+        // Поле подтверждение пароля
+        passwordConfirmField = new JPasswordField();
+        passwordConfirmField.setEchoChar('\u0000'); // Делаем текст видимым
+        passwordConfirmField.setText(INPUT_PASS_CONFIRM_TEXT);
+        passwordConfirmField.setVisible(false);
+        add(passwordConfirmField);
+
+        // Селектор
+        JPanel rButtonsPanel = new JPanel();
+        rButtonsPanel.setLayout(new BoxLayout(rButtonsPanel, BoxLayout.X_AXIS));
+
+        ButtonGroup rButtonGroup = new ButtonGroup();
+        JRadioButton logInRButton = new JRadioButton("Вход");
+        JRadioButton joinRButton = new JRadioButton("Регистрация");
+        rButtonGroup.add(logInRButton);
+        rButtonGroup.add(joinRButton);
+        logInRButton.setSelected(true);
+        rButtonsPanel.add(logInRButton);
+        rButtonsPanel.add(joinRButton);
+        add(rButtonsPanel);
+
+        // Кнопка
+        buttonLogIn = new JButton();
+        buttonLogIn.setAlignmentX(CENTER_ALIGNMENT);
+        buttonLogIn.setText("Войти");
+        //buttonLogIn.setFocusPainted(false);
+        //buttonLogIn.requestFocusInWindow();
+        add(buttonLogIn);
 
         setVisible(true);
 
@@ -77,18 +102,62 @@ public class LoginWindow extends JFrame implements ClientUI {
             }
         });
 
+        passwordConfirmField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if( new String( passwordConfirmField.getPassword() ).equals(INPUT_PASS_CONFIRM_TEXT) ) {
+                    passwordConfirmField.setText("");
+                    passwordConfirmField.setEchoChar('*');
+                }
+            }
 
-        buttonIn.addActionListener(e -> {
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(passwordConfirmField.getPassword().length == 0) {
+                    passwordConfirmField.setEchoChar('\u0000'); // Делаем текст видимым
+                    passwordConfirmField.setText(INPUT_PASS_CONFIRM_TEXT);
+                }
+            }
+        });
+
+        logInRButton.addActionListener(e -> {
+            passwordConfirmField.setVisible(false);
+            this.setTitle("Вход");
+            this.revalidate();
+        });
+
+        joinRButton.addActionListener(e -> {
+            passwordConfirmField.setVisible(true);
+            this.setTitle("Регистрация");
+            this.revalidate();
+        });
+
+        buttonLogIn.addActionListener(e -> {
             String nickName = nickNameField.getText();
             String pass = new String( passwordField.getPassword());
-            if( nickName.equals(INPUT_NICK_TEXT) || pass.equals(INPUT_PASS_TEXT) ) {
-                JOptionPane.showMessageDialog(null, "Заполните оба поля!");
-                return;
+            String passConfirm = new String( passwordConfirmField.getPassword());
+
+            if(logInRButton.isSelected()) {
+                if (nickName.equals(INPUT_NICK_TEXT) || pass.equals(INPUT_PASS_TEXT)) {
+                    JOptionPane.showMessageDialog(null, "Заполните оба поля!");
+                    return;
+                }
+            } else {
+                if (nickName.equals(INPUT_NICK_TEXT) || pass.equals(INPUT_PASS_TEXT)) {
+                    JOptionPane.showMessageDialog(null, "Заполните оба поля!");
+                    return;
+                }
+
+                if(!pass.equals(passConfirm)) {
+                    JOptionPane.showMessageDialog(null, "Пароль и подтверждение пароля не совпадают!");
+                    return;
+                }
             }
 
             nickNameField.setEnabled(false);
             passwordField.setEnabled(false);
-            buttonIn.setEnabled(false);
+            passwordConfirmField.setEnabled(false);
+            buttonLogIn.setEnabled(false);
             clientController.logIn(nickName, pass);
         });
     }
@@ -111,7 +180,8 @@ public class LoginWindow extends JFrame implements ClientUI {
 
         nickNameField.setEnabled(true);
         passwordField.setEnabled(true);
-        buttonIn.setEnabled(true);
+        passwordConfirmField.setEnabled(true);
+        buttonLogIn.setEnabled(true);
     }
 
     @Override
